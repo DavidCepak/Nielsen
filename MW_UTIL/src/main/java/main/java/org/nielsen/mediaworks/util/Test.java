1975a0911fa29939164bdcebc114cc955777c637
David <david.cepak@gmail.com>
2020-02-28 19:41:06 +0100

    Testiram

---
+++ MW_UTIL/src/main/java/main/java/org/nielsen/mediaworks/util/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.org.nielsen.mediaworks.util;
+
+/**
+ *
+ * @author vape8001
+ */
+public class Test {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("...");
+        System.out.println("test");
+    }
+    
+}
